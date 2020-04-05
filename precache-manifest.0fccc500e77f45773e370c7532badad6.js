c65760947b335ceffa2c3a19f384bfa742bfad23
Serdar Mustafa <serdarmustafa@Serdar-Personal-MBP.local>
2020-04-05 19:32:52 +0300

    Updates

---
+++ precache-manifest.0fccc500e77f45773e370c7532badad6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f64911dbac2b6209a9048d8a1e2feaad",
+    "url": "/veriff-tmdb/index.html"
+  },
+  {
+    "revision": "ef7c422e8e7b8c54be43",
+    "url": "/veriff-tmdb/static/css/2.a8477468.chunk.css"
+  },
+  {
+    "revision": "c6bdb01ab3982a52e881",
+    "url": "/veriff-tmdb/static/css/main.6e5daeb9.chunk.css"
+  },
+  {
+    "revision": "ef7c422e8e7b8c54be43",
+    "url": "/veriff-tmdb/static/js/2.2e428122.chunk.js"
+  },
+  {
+    "revision": "c6bdb01ab3982a52e881",
+    "url": "/veriff-tmdb/static/js/main.eba2cb9b.chunk.js"
+  },
+  {
+    "revision": "df16df6a2ccc0ceb95eb",
+    "url": "/veriff-tmdb/static/js/runtime-main.f37c3749.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/veriff-tmdb/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/veriff-tmdb/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/veriff-tmdb/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/veriff-tmdb/static/media/slick.f97e3bbf.svg"
+  }
+]);
