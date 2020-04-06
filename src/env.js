b1ac6673cd649a32f59d60ea5a4d869d09f26edc
Serdar Mustafa <serdarmustafa@Serdar-Personal-MBP.local>
2020-04-06 15:50:16 +0300

    some changes for heroku

---
+++ src/env.js
@@ -0,0 +1,7 @@
+// Configuration for TMDB from https://api.themoviedb.org/3/configuration?api_key=687f4af88c405c0b881295bba3d4adfc
+
+const API_URL = "https://api.themoviedb.org/3/";
+const API_KEY = "687f4af88c405c0b881295bba3d4adfc";
+const VERIFF_KEY = "394b1f03-204c-449c-bb45-98cc44ceabdb";
+
+export { API_URL, API_KEY, VERIFF_KEY };
